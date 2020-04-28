b2d11be5e4a6f236194a95179835155d1e93580f
NataliyHusak <50527604+NataliyHusak@users.noreply.github.com>
2020-04-28 23:19:33 +0300

    01

--- contacts.js
+++ contacts.js
@@ -1,76 +1,95 @@
 const fs = require("fs");
 const path = require("path");
+const { promises: fsPromises } = fs;
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// TODO: задокументировать каждую функцию
-
-function listContacts() {
-  fs.readFile(contactsPath, (error, data) => {
-    if (error) {
-      throw error;
-    }
-    console.table(JSON.parse(data));
-  });
+function getData() {
+  return fsPromises
+    .readFile(contactsPath, "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((err) => err);
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, (error, data) => {
-    if (error) {
-      throw error;
-    }
-    const found = JSON.parse(data).find((el) => el.id === contactId);
-    console.log(found);
-  });
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath, (err, contacts) => {
-    if (err) {
+function listContacts(req, res) {
+  getData()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
       console.log(err);
-      return;
-    }
-
-    const contactsList = JSON.parse(contacts);
-
-    const filtredContacts = contactsList.filter(
-      (contact) => contact.id !== contactId
-    );
-
-    fs.writeFile(contactsPath, JSON.stringify(filtredContacts), (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Contacts removed");
     });
-  });
 }
 
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, (err, contacts) => {
-    if (err) {
+function getContactById({ req, res, contactId }) {
+  getData()
+    .then((data) => {
+      const contact = data.find((el) => el.id == contactId);
+      if (contact) {
+        res.send(contact);
+      } else {
+        res.status(404).send({ message: "Not found" });
+      }
+    })
+    .catch((err) => {
       console.log(err);
-      return;
-    }
-    const arr = JSON.parse(contacts);
-    const latsItem = arr[arr.length - 1];
+    });
+}
 
+function removeContact({ res, contactId }) {
+  getData()
+    .then((data) => {
+      const contact = data.find((item) => item.id == contactId);
+      if (!contact) {
+        res.status(404).send({ message: "Not found" });
+      }
+      const filteredContacts = data.filter((item) => item.id != contactId);
+      fsPromises
+        .writeFile(contactsPath, JSON.stringify(filteredContacts, "", 2))
+        .then(() => {
+          res.status(200).send({ message: "contact deleted" });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+//********* */
+
+function addContact({ name, email, phone, res }) {
+  getData().then((data) => {
     const contact = {
-      id: latsItem.id + 1,
+      id: _randomInteger(),
       name,
       email,
       phone,
     };
+    data.push(contact);
+    fsPromises
+      .writeFile(contactsPath, JSON.stringify(data, "", 2))
+      .then(() => {
+        res.status(201).send(contact);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+}
 
-    arr.push(contact);
-    fs.writeFile(contactsPath, JSON.stringify(arr), (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Contacts Added");
-    });
+function updateContact({ req, res, id }) {
+  getData().then((data) => {
+    const contact = data.findIndex((item) => item.id == id);
+    if (contact == -1) {
+      res.status(404).send({ message: "Not found" });
+    } else {
+      Object.assign(data[contact], { ...req.body });
+      fsPromises
+        .writeFile(contactsPath, JSON.stringify(data, null, 2))
+        .then(() => {
+          res.send(data[contact]);
+        });
+    }
   });
 }
 
@@ -79,4 +98,5 @@ module.exports = {
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 };
--- index.js
+++ index.js
@@ -1,29 +1,80 @@
-const argv = require("yargs").argv;
+const express = require("express");
+const cors = require("cors");
+const Joi = require("joi");
+// const dotenv = require("dotenv");
 const contacts = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      console.table(contacts.listContacts());
-      break;
+const app = express();
 
-    case "get":
-      console.log(contacts.getContactById(id));
-      break;
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded());
 
-    case "add":
-      contacts.addContact(name, email, phone);
-      console.log(contacts.listContacts);
-      break;
+// dotenv.config();
 
-    case "remove":
-      contacts.removeContact(id);
-      console.log(contacts.listContacts);
-      break;
+// const PORT = process.env.PORT;
+const PORT = 3002;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+app.get("/", (req, res) => {
+  res.send("Hello from api");
+});
+
+app.get("/api/contacts", (req, res) => {
+  contacts.listContacts(req, res);
+});
+
+app.get("/api/contacts/:contactId", (req, res) => {
+  contacts.getContactById({ req, res, contactId: req.params.contactId });
+});
+
+app.delete("/api/contacts/:contactId", (req, res) => {
+  const contactId = req.params.contactId;
+  contacts.removeContact({ res, contactId });
+});
+
+//********* */
+app.post(
+  "/api/contacts",
+  (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      phone: Joi.string().required(),
+    });
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+      res.status(400).send(result.error);
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    contacts.addContact({ ...req.body, res });
   }
-}
+);
 
-invokeAction(argv);
+app.patch(
+  "/api/contacts/:contactId",
+  (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string(),
+      email: Joi.string(),
+      phone: Joi.string(),
+    });
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+      resp.status(400).send(result.error);
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    const id = req.params.contactId;
+    contacts.updateContact({ req, res, id });
+  }
+);
+
+app.listen(PORT, () => {
+  console.log("App start on port:", PORT);
+});
